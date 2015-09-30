9c421b52c816a2a46de1cdcb598ddc05caacf72f
Sudhanva <sudhanvaah@users.noreply.github.com>
2015-09-30 12:06:49 +0530

    .

--- sjf.cpp
+++ sjf.cpp
@@ -11,13 +11,13 @@ main()
 	{
         cout<<"\nEnter burst time of P"<<i<<" ";
         cin>>burst[i];
-		pno[i]=i;		// storing order of processes/process name
+		pno[i]=i;	
 	}
     for(i=0; i<n; i++)
     {
         for(j=i+1; j<n; j++)
         {
-            if(burst[i]>burst[j])		// sorting based on the burst time of processes
+            if(burst[i]>burst[j])	
             {
 			temp=burst[i];
 			burst[i]=burst[j];
@@ -28,15 +28,13 @@ main()
             }
         }
     }
-    fintime[0]=burst[0]; 		// time at which process 0 finished execution;In fcfs,we had r[0]=burst[0]+a[0],but in this case we don't have arrival time.
+    fintime[0]=burst[0]; 		
     for(i=1;i<n;i++)
-                           // if next process arrives instantly at the time at which previous ends
-        fintime[i]=fintime[i-1]+burst[i]; //time at which previous process finished execution+the burst time of this process gives the finish time of current process
-                         // No second case as in fcfs
-    wait[0]=0;             // waiting time of first process is 0
-    for(i=1;i<n;i++)   //now we calculate the waiting time for other processes
-        wait[i]=fintime[i-1];  // process i waits till process i-1 finishes its execution, so this is its waiting time
-    for(i=0;i<n;i++) // Turnaround time
+        fintime[i]=fintime[i-1]+burst[i];
+    wait[0]=0;
+    for(i=1;i<n;i++)
+        wait[i]=fintime[i-1];
+    for(i=0;i<n;i++)
         turnaround[i]=burst[i]+wait[i];
     for(i=0;i<n;i++)
     {
